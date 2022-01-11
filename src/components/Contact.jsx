f43bd3991515d7e1725ce72ffa884b4769ed6e42
Juanka <valerojuanka@gmail.com>
2022-01-11 02:38:55 -0300

    form styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./contact.css";
 import Title from "./Title";
 
 const Contact = () => {
@@ -6,12 +6,30 @@ const Contact = () => {
     <div className="container">
       <Title title="Contact" />
       <div className="contact_container">
-          <div className="form_container">
-
-          </div>
-          <div className="desc_container">
-              
-          </div>
+        <div className="form_container">
+          <form className="form">
+            <p>
+              <label>Nombre:</label>
+              <input type="text" />
+            </p>
+            <p>
+              <label>Email:</label>
+              <input type="email" />
+            </p>
+            <p>
+              <label>Asunto:</label>
+              <input type="text" />
+            </p>
+            <p>
+              <label>Tu Mensaje:</label>
+              <textarea  rows="3"></textarea>
+            </p>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+        <div className="desc_container">
+          <h4>Mensaje</h4>
+        </div>
       </div>
     </div>
   );
